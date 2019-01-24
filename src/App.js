0f4621925892640d5c3b2ec3fed954014c8baccc
Tim Weiss <tdweiss1978@gmail.com>
2019-01-24 12:13:39 -0800

    Firebase Installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList './components/RoomList';
+
+  var config = {
+    apiKey: "AIzaSyB-ujG5PJJIkbLDIdaINNsXEU_a-Gkos3M",
+    authDomain: "bloc-chat-abf35.firebaseapp.com",
+    databaseURL: "https://bloc-chat-abf35.firebaseio.com",
+    projectId: "bloc-chat-abf35",
+    storageBucket: "bloc-chat-abf35.appspot.com",
+    messagingSenderId: "352609805270"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
