5959e5b51ccb98638a929d7ad05d3db3c5e204b9
Tim Weiss <tdweiss1978@gmail.com>
2019-01-25 16:43:57 -0800

    List of Chat Rooms Added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import * as firebase from 'firebase';
-import RoomList './components/RoomList';
+import RoomList from './Components/RoomList';
 
-  var config = {
-    apiKey: "AIzaSyB-ujG5PJJIkbLDIdaINNsXEU_a-Gkos3M",
-    authDomain: "bloc-chat-abf35.firebaseapp.com",
-    databaseURL: "https://bloc-chat-abf35.firebaseio.com",
-    projectId: "bloc-chat-abf35",
-    storageBucket: "bloc-chat-abf35.appspot.com",
-    messagingSenderId: "352609805270"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyB-ujG5PJJIkbLDIdaINNsXEU_a-Gkos3M",
+  authDomain: "bloc-chat-abf35.firebaseapp.com",
+  databaseURL: "https://bloc-chat-abf35.firebaseio.com",
+  projectId: "bloc-chat-abf35",
+  storageBucket: "bloc-chat-abf35.appspot.com",
+  messagingSenderId: "352609805270"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Welcome to<br /> Bloc Chat Room</h1>
         </header>
+        <main>
+          <RoomList firebase={firebase} />
+        </main>
       </div>
     );
   }
