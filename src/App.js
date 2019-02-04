543f439f1d952b607673c829c486d9b8505e5d22
Tim Weiss <tdweiss1978@gmail.com>
2019-02-04 11:26:34 -0800

    List Messages Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 //App
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './Components/RoomList';
+import MessageList from './Components/MessageList';
 
 var config = {
   apiKey: "AIzaSyB-ujG5PJJIkbLDIdaINNsXEU_a-Gkos3M",
@@ -16,16 +16,44 @@ var config = {
 firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeRoom: "",
+      activeRoomId: "",
+      username: ""
+
+    }
+  }
+
+  setActiveRoom = room => {
+    this.setState({ activeRoom: room});
+    this.setState({ activeRoomId: room.key});
+    this.setState({ username: room.username});
+    }
+
   render() {
     return (
       <div className="App">
         <header>
-          <h1>Welcome to<br /> Bloc Chat Room</h1>
+          <h1>Welcome to<br /> Bloc Chat</h1>
         </header>
         <main>
-          <RoomList firebase={firebase} />
-        </main>
-      </div>
+          <RoomList
+            firebase={firebase}
+            activeRoom={this.state.activeRoom}
+            setActiveRoom={this.setActiveRoom}
+            isActiveRoom={this.state.isActiveRoom}
+            activeRoomId={this.state.activeRoomId}
+          />
+          <MessageList
+            firebase={firebase}
+            activeRoom={this.state.activeRoom}
+            activeRoomId={this.state.activeRoomId}
+            username={this.state.username}
+          />
+       </main>
+     </div>
     );
   }
 }
