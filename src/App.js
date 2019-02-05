36f9ddd40972f3ec3b021ba3540a12b81de48d19
Tim Weiss <tdweiss1978@gmail.com>
2019-02-05 14:08:41 -0800

    Set Username Complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './Components/RoomList';
 import MessageList from './Components/MessageList';
+import User from './Components/User';
 
 var config = {
   apiKey: "AIzaSyB-ujG5PJJIkbLDIdaINNsXEU_a-Gkos3M",
@@ -21,8 +22,8 @@ class App extends Component {
     this.state = {
       activeRoom: "",
       activeRoomId: "",
-      username: ""
-
+      username: "",
+      currentUser: ""
     }
   }
 
@@ -30,6 +31,10 @@ class App extends Component {
     this.setState({ activeRoom: room});
     this.setState({ activeRoomId: room.key});
     this.setState({ username: room.username});
+    }
+
+    setUser(user) {
+      this.setState({ currentUser: user })
     }
 
   render() {
@@ -45,12 +50,19 @@ class App extends Component {
             setActiveRoom={this.setActiveRoom}
             isActiveRoom={this.state.isActiveRoom}
             activeRoomId={this.state.activeRoomId}
+            setUser={this.setUSer}
           />
           <MessageList
             firebase={firebase}
             activeRoom={this.state.activeRoom}
             activeRoomId={this.state.activeRoomId}
             username={this.state.username}
+            setUser={this.setUSer}
+          />
+          <User
+            firebase={firebase}
+            username={this.state.username}
+            setUser={this.setUSer}
           />
        </main>
      </div>
---
+++ src/Components/User.js
@@ -0,0 +1,47 @@
+//User
+import React, { Component } from 'react';
+
+class User extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user);
+   });
+  }
+
+  handleSignIn(e) {
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+    });
+  }
+
+  handleSignOut(e){
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signOut();
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={ (e) => this.handleSignIn(e) }>Sign-In</button>
+        <button onClick={ (e) => this.handleSignOut(e) }>Sign-Out</button>
+        <p>User: {this.props.user? this.props.user.displayName : "Guest"} </p>
+     </div>
+    )
+  }
+}
+
+
+
+
+export default User;
